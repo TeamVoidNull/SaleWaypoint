e1417854f7622f3ad34256d28a32e16ff9cfc347
Peters-Joseph <petersjl@rose-hulman.edu>
2021-04-21 23:10:05 -0400

    Fixed ravendb class

--- public/src/data/Interfaces/RavenDB.js
+++ public/src/data/Interfaces/RavenDB.js
@@ -1,31 +1,43 @@
+import { DocumentStore } from "ravendb";
+
 class RavenDB{
-    constructor(store, database){
-        this.store = store;
+
+    static instance;
+
+    store;
+    database;
+    session;
+
+    constructor(database){
+        if(this.instance) return;
+        this.instance = this;
+
+        this.store = new DocumentStore("http://137.112.89.84:8080", "MyDistrubutedDB");
+        store.initialize();
         this.database = database;
+        this.session = this.store.openSession(this.database)
     }
 
     async insertGame(game){
-        const session = this.store.openSession(this.database)
-        await session.store(game)
-        await session.saveChanges();
+        await this.session.store(game)
+        await this.session.saveChanges();
     }
 
     async getGame(gameId){ //Look in to loading
-        const session = this.store.openSession(this.database)
         const myQuery = session.query({collection: 'Games'}).whereEquals('uid', gameId);
         const game = await myQuery.firstOrNull();
         return game
     }
 
     async deleteGame(gameId){
-        const session = documentStore.openSession();
-        session.delete(gameId);
-        await session.saveChanges();
+        this.session.delete(gameId);
+        await this.session.saveChanges();
     }
 
     async listGames(){
         const results = await session
             .query({collection: 'Games'})
             .all(); // send query
+        return results;
     }
 }
--- public/src/data/Persistance/RavenDB/documentStore.js
+++
@@ -1,5 +0,0 @@
-import { DocumentStore } from "ravendb";
-
-const store = new DocumentStore("http://137.112.89.84:8080", "MyDistrubutedDB");
-store.initialize();
-export { store as documentStore }; 
