1d3f392ae0e5867fe1845b731ea0b47173d3c038
Peters-Joseph <petersjl@rose-hulman.edu>
2021-04-23 08:16:39 -0400

    Fix for cors preflight

--- express.js
+++ express.js
@@ -24,6 +24,8 @@ app.post('/addGame', async function(req, res){
     await session.store(req.body)
     await session.saveChanges();
     res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "POST")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
     res.send("Got your game");
 })
 
