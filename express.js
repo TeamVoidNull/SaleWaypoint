c777bb64f75079b3ac6f1b4045031e020287d136
Peters-Joseph <petersjl@rose-hulman.edu>
2021-04-23 04:50:10 -0400

    Added game list "GET" call to server

--- express.js
+++ express.js
@@ -1,6 +1,19 @@
-var express = require("express")
+var express = require("express");
 var app = express();
+var raven = require("ravendb");
 
-app.use('/', express.static("public") );
+let database = "MyDistrubutedDB";
+let store = new raven.DocumentStore("http://137.112.89.84:8080", "MyDistrubutedDB");
+store.initialize();
+let session = store.openSession(database);
+
+app.use('/', express.static("./public") );
+
+app.get('/getGamesList', async function(req, res){
+    console.log("Recieved game list request")
+    let results = await session.query({collection: "Games"}).all()
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.send(results);
+})
 
 app.listen(3000);
