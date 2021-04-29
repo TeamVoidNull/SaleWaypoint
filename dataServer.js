dac1d345eb1954483d8ea7a6df777c07abd5f6e7
Trey <srkline3@gmail.com>
2021-04-29 10:51:43 -0400

    add game with redis?

--- dataServer.js
+++ dataServer.js
@@ -21,21 +21,34 @@ app.use('/', express.static("./public") );
 app.use('/addGame', bodyParser.json())
 
 app.get('/getGamesList', async function(req, res){
-    // console.log("Recieved game list request")
-    // let results = await session.query({collection: "Games"}).all()
-    // res.setHeader("Access-Control-Allow-Origin", "*")
-    // res.send(results);
+    console.log("Recieved game list request")
+    let results = await session.query({collection: "Games"}).all()
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.send(results);
 })
 
 app.post('/addGame', async function(req, res){
     console.log("Recieved add game request");
-    // console.log(req.body)
-    // await session.store(req.body)
-    // await session.saveChanges();
-    // res.setHeader("Access-Control-Allow-Origin", "*")
-    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    // res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-    // res.send("Got your game");
+    console.log(req.body)
+    newGameId = uuidv4()
+    var newGame = JSON.parse(req.body)
+
+    await session.store(req.body, newGameId)
+    await redisClient.lpush(newGameId, newGame.title)
+
+    await session.saveChanges();
+   
+    
+
+
+    //redis insert
+    var newGame = JSON.parse(req.body)
+    
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+    res.send("Got your game");
 })
 
 app.options('/addGame', async function(req, res){
@@ -45,11 +58,11 @@ app.options('/addGame', async function(req, res){
     res.send();
 })
 
-// function deleteGame(game){
-//     console.log("Calling delete");
-//     session.delete(game);
-//     session.saveChanges();
-// }
+function deleteGame(game){
+    console.log("Calling delete");
+    session.delete(game);
+    session.saveChanges();
+}
 
 
 
@@ -61,3 +74,10 @@ let test = false;
 let frontUrl = test ? testUrl : mainUrl;
 
 app.listen(3001);
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
