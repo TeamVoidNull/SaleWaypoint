264cae4fe76071b0f4582ffe86911d52ebd804f8
Peters-Joseph <petersjl@rose-hulman.edu>
2021-05-20 12:51:19 -0400

    Added cors for searching

--- server.js
+++ server.js
@@ -763,6 +763,13 @@ app.options('/updateGame', async function(req, res){
     res.send();
 })
 
+app.options('/searchByName', async function(req, res){
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    res.send();
+})
+
 function deleteGame(game){
     console.log("Calling delete");
     ravenSession.delete(game);
